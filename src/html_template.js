0348957ad9ba9a8061e3fd4cd5dc0ee833a2cb90
Mackenzie Gray <mdkulen@gmail.com>
2022-07-22 13:32:12 +1000

    added styling to generated page

--- src/html_template.js
+++ src/html_template.js
@@ -2,8 +2,8 @@ const generateTeamProfile = (teamData) => {
     // function to generate card for manager
     const generateManager = (manager) => {
         return `
-        <div class="card employee-card">
-            <div class="card-header">
+        <div class="card employee-card shadow p-3 mb-5 bg-body rounded">
+            <div class="card-header bg-primary text-white">
                 <h2 class="card-title">${manager.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
             </div>
@@ -21,8 +21,8 @@ const generateTeamProfile = (teamData) => {
     // function to generate card for engineer
     const generateEngineer = (engineer) => {
         return `
-        <div class="card employee-card">
-            <div class="card-header">
+        <div class="card employee-card shadow p-3 mb-5 bg-body rounded">
+            <div class="card-header bg-primary text-white">
                 <h2 class="card-title">${engineer.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
             </div>
@@ -40,8 +40,8 @@ const generateTeamProfile = (teamData) => {
     // function to generate card for intern
     const generateIntern = (intern) => {
         return `
-        <div class="card employee-card">
-            <div class="card-header">
+        <div class="card employee-card shadow p-3 mb-5 bg-body rounded">
+            <div class="card-header bg-primary text-white">
                 <h2 class="card-title">${intern.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
             </div>
@@ -91,25 +91,27 @@ module.exports = (teamData) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Work Team</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="./dist/style.css">        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">        
+        <link rel="stylesheet" href="../dist/style.css">        
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Work Team</h1>
+        <div class="container-fluid text-bg-danger">
+            <div id="header-row" class="row align-items-center">
+                <div class="col-12 jumbotron mb-3 team-heading text-bg-danger p-3">
+                    <h1 class="text-center fs-1">My Awesome Work Team</h1>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
+                <div class="team-area col-12 mt-5 d-flex justify-content-evenly">
                     ${generateTeamProfile(teamData)}
                 </div>
             </div>
         </div>
+
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </body>
     </html>
     `;
