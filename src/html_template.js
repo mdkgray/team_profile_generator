822b8a318fd96297e476fd49a413ab370b108f23
Mackenzie Gray <mdkulen@gmail.com>
2022-07-22 14:05:42 +1000

    updated html

--- src/html_template.js
+++ src/html_template.js
@@ -112,7 +112,7 @@ module.exports = (teamData) => {
 
         <script src="https://kit.fontawesome.com/c502137733.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    </bodyclass=>
+    </body>
     </html>
     `;
 };
